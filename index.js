6eb702373fb0fca8ebeca74651c58ce591fbb04e
yenifergalarza <yeniferalexandra2000@hotmail.com>
2018-08-16 17:37:57 -0500

    ahora tiene su linea de comando y comprueba si es un archivo md

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+#!/usr/bin/env node
 const fs = require('fs');
-fs.readFile('README.md','utf8',(err,buff)=>{
-    console.log(buff.toString());
-});
-console.log('no se pudo leer el md');
+const path = require('path');
+
+
+const [, , ...args] = process.argv
+
+const check = () => {
+
+	if (path.extname(`${args}`) !== '.md') {
+		console.log('no se pudo leer el archivo');
+	} 
+	
+	else{
+		const func = () => {
+			console.log(`Hola soy Yenifer${args}`);
+
+			fs.readFile(args[0], 'utf8', (err, buff) => {
+				console.log(buff.toString());
+				console.log('entre a fs');
+			});
+		}
+			func();
+
+	} 
+}
+check()
+
+
+
+
+//console.log(process.argv)
+console.log(args)
---
+++ test.js
@@ -0,0 +1,7 @@
+const init = () => {
+    console.log('Hola mundo!');
+};
+
+init();
+
+process.exit(1);// 
