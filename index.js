8a55ebec4e008c5f2bfa2178a8dc2b078f17347c
yenifergalarza <yeniferalexandra2000@hotmail.com>
2018-08-20 19:29:18 -0500

    regex funciona

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-
+const validate = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
 
 const [, , ...args] = process.argv
-
+let md = `${args}`
 const check = () => {
 
-	if (path.extname(`${args}`) !== '.md') {
+	if (path.extname(md) !== '.md') {
 		console.log('no se pudo leer el archivo');
-	} 
-	
-	else{
+	}
+
+	else {
 		const func = () => {
-			console.log(`Hola soy Yenifer${args}`);
+			console.log(`Hola soy md`);
 
 			fs.readFile(args[0], 'utf8', (err, buff) => {
-				console.log(buff.toString());
+				// console.log(buff.toString());
 				console.log('entre a fs');
+				let str = buff.toString();
+				console.log(typeof str);
+				let equal = str.match(validate)  ;
+				console.log(equal); 
+
+				//var result = validate.exec (str);
+               //console.log(result);
+
 			});
 		}
-			func();
+		func();
 
-	} 
+	}
 }
-check()
 
 
+check();
 
+
+// 
 
 //console.log(process.argv)
-console.log(args)
