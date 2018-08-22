91f1aacfde0c0a1aa54ffd8cc6bae264c71500f4
yenifergalarza <yeniferalexandra2000@hotmail.com>
2018-08-22 18:37:24 -0500

    lee directorio identifica links y archivos

--- index.js
+++ index.js
@@ -2,39 +2,78 @@
 const fs = require('fs');
 const path = require('path');
 const validate = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+const validateLink = /\[[\w\s]*\](\((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
 
 const [, , ...args] = process.argv
-let md = `${args}`
-const check = () => {
+let md = `${args}`;
+const mdLinks = () => {
+	fs.stat(md, function (err, stats) {
 
-	if (path.extname(md) !== '.md') {
-		console.log('no se pudo leer el archivo');
-	}
-
-	else {
-		const func = () => {
-			console.log(`Hola soy md`);
-
-			fs.readFile(args[0], 'utf8', (err, buff) => {
-				// console.log(buff.toString());
-				console.log('entre a fs');
-				let str = buff.toString();
-				console.log(typeof str);
-				let equal = str.match(validate)  ;
-				console.log(equal); 
-
-				//var result = validate.exec (str);
-               //console.log(result);
-
-			});
+		if (stats.isFile()) {
+			console.log('    file');
+			func(md);
 		}
-		func();
+		if (stats.isDirectory()) {
+			console.log('    directory');
+			leerCarpeta(md);
+		}
+	})
 
-	}
+}
+
+const func = (md) => {
+	console.log(`Hola soy md`);
+
+	fs.readFile(md, 'utf8', (err, buff) => {
+
+		if (path.extname(md) !== '.md') {
+			console.log('no se pudo leer el archivo');
+		}
+	
+		else {
+		// console.log(buff.toString());
+		console.log('entre a fs');
+		let str = buff.toString();
+		console.log(typeof str);
+		let equal = str.match(validate);
+		console.log(equal);
+
+
+		//console.log(Array.isArray(equal));
+		let equalLink = str.match(validateLink);
+		console.log(equalLink);
+
+		//var result = validate.exec (str);
+		//console.log(result);
+
+		}
+	});
+}
+	
+const leerCarpeta = (md) => {
+	fs.readdir(md, (err, data) => {
+		console.log("estas aqui");
+		console.log(data)
+	})
 }
 
 
-check();
+
+
+const check = () => {
+
+	if (path.extname(md) !== '.md') {
+		leerCarpeta(md);
+	} else {
+
+		func(md);
+
+	}
+
+}
+
+
+mdLinks();
 
 
 // 
